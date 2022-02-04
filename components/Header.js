18db3184d19a22d39a651690b7137c10253b4fef
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-04 17:34:57 +0700

    made icon and animate

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,25 @@
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
     return (
-        <header className="">
-            <div>
-                <HeaderItem title='Home' Icon={} />
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
             </div>
-            <h1>This header</h1>
             {/* This are image optimationz from next */}
             <Image
                 className="object-contain"
